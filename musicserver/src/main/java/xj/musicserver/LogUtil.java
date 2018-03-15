6609d4443e06a42097cff8615be1d5519b3d0dcd
JustinXf <xiefengzel@sohu.com>
2018-03-15 12:43:09 +0800

    commit4

--- musicserver/src/main/java/xj/musicserver/LogUtil.java
+++ musicserver/src/main/java/xj/musicserver/LogUtil.java
@@ -8,6 +8,7 @@ import android.util.Log;
  * 
  */
 public class LogUtil {
+    
     public static final String TAG = "MusicInterface";
     public static final int VERBOSE = 1;
     public static final int DEBUG = 2;
